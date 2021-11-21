14110dabbc3d0295e85c4b753502b47418b6aa12
Phi Vu <hoalong369741@gmail.com>
2021-11-21 17:35:49 +0700

    test16

--- models/likedsong.js
+++ models/likedsong.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose'); //goi mongoose 
-const schema = mongoose.Schema({ //tao ra mot định hình dữ liệu cho user
+const likedschema = mongoose.Schema({ //tao ra mot định hình dữ liệu cho user
     songName: String,
     imageSongURL: String,
 }, {
     version: false,
     collection: 'liked-songs'
 })
-const LikedSongModel = mongoose.model("liked-songs", schema); //tạo ra đối tượng ứng với cách định hình ở trên
+const LikedSongModel = mongoose.model("liked-songs", likedschema); //tạo ra đối tượng ứng với cách định hình ở trên
 module.exports = LikedSongModel;
